50430a156b56b1d0a4a4045a2608ae1fb6b888b6
tomasz.mazalewski <tomaszmazalewski@gmail.com>
2024-09-19 11:31:57 +0200

    alpha 1.0

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ baza = Database(host="localhost", user="root", password="", database="notatnik")
 
 baza.create_tables()
 
-
+##dsdsd
 def rejestracja():
     login = login_entry.get().strip()
     password = haslo_entry.get().strip()
