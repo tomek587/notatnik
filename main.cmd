434cb736f05837d7bf3e4527a87cdb849c6fa8c8
tomek587 <tomaszmazalewski@gmail.com>
2024-09-20 13:05:48 +0200

    create exe

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=C:\Users\Admin\AppData\Local\Programs\Python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\Admin\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\Admin\Documents\python\notatnik;C:\Users\Admin\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\Admin\AppData\Local\Programs\Python\Python312\Lib;C:\Users\Admin\AppData\Local\Programs\Python\Python312;C:\Users\Admin\Documents\python\notatnik\.venv;C:\Users\Admin\Documents\python\notatnik\.venv\Lib\site-packages;C:\Users\Admin\AppData\Local\Programs\Python\Python312\Lib\site-packages;C:\Users\Admin\Documents\python\notatnik\.venv\Lib\site-packages\setuptools\_vendor
+"%~dp0main.exe" %*
