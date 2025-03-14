240fbe998d95c31fc2cf29fe5eb5492537a39a1d
tomek857 <tomaszmazalewski@gmail.com>
2025-03-14 10:10:47 +0100

    main23

---
+++ test_baza.py
@@ -0,0 +1,30 @@
+from unittest import TestCase
+
+
+class TestDatabase(TestCase):
+    def test_create_tables(self):
+        self.fail()
+
+    def test_check_user(self):
+        self.fail()
+
+    def test_check_user_exists(self):
+        self.fail()
+
+    def test_insert_user(self):
+        self.fail()
+
+    def test_get_user_id(self):
+        self.fail()
+
+    def test_select_notatki_by_user(self):
+        self.fail()
+
+    def test_insert_notatka(self):
+        self.fail()
+
+    def test_delete_notatka(self):
+        self.fail()
+
+    def test_close(self):
+        self.fail()
---
+++ test_main.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestMain(TestCase):
+    def test_logowanie(self):
+        self.fail()
+
+    def test_otworz_notatnik(self):
+        self.fail()
+
+    def test_wyswietl_notatki(self):
+        self.fail()
+
+    def test_wyswietl_zaznaczona_notatka(self):
+        self.fail()
+
+    def test_dodaj_notatke(self):
+        self.fail()
+
+    def test_usun_wybrana_notatka(self):
+        self.fail()
+
+    def test_wyloguj(self):
+        self.fail()
+
+    def test_zaladuj_okno_logowania(self):
+        self.fail()
